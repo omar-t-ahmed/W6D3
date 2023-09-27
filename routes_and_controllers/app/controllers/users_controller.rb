438772abfd98d979af6ed5c4aaa062a10c4482c1
omar-t-ahmed <omartahmed@gmail.com>
2023-09-27 13:57:46 -0400

    finish phase 2 and 3

---
+++ routes_and_controllers/app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+
+class UsersController < ApplicationController
+
+    def index
+        render plain: "I'm in the index action!"
+    end
+
+    def create
+        render json: params
+    end
+
+end
