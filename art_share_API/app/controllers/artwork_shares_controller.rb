6adf7f686121e4dc0bb3e2a37060631b731739a4
Akash Pulluru <akashpulluru@gmail.com>
2023-09-28 11:37:28 -0400

    partway through part three

--- art_share_API/app/controllers/artwork_shares_controller.rb
+++ art_share_API/app/controllers/artwork_shares_controller.rb
@@ -1,3 +1,30 @@
 class ArtworkSharesController < ApplicationController
     
+
+    def create
+        artwork_share = ArtworkShare.new(artwork_share_params)
+
+        if artwork_share.save
+            render json: artwork_share
+        else 
+            render plain: 'Artwork Share was not successfully created!'
+        end
+
+    end 
+
+    def destroy
+        artwork_share = ArtworkShare.find_by(id: params[:id])
+
+        if artwork_share && artwork_share.destroy
+            render json: artwork_share
+        else
+            raise plain: 'artwork share does not exist'
+        end
+    end
+
+    private 
+
+    def artwork_share_params
+            params.require(:artwork_share).permit(:viewer_id, :artwork_id)
+    end 
 end
